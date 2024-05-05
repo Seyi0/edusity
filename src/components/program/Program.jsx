472a75477a422c766f6bdbd424f5bca49473355a
Seyi0 <makanjuolaseyi560@gmail.com>
2024-05-05 18:52:06 +0100

    :art:

--- src/components/program/Program.jsx
+++ src/components/program/Program.jsx
@@ -1,11 +1,22 @@
-
+import './Program.css'
+import program_1 from '../../assets/program-1.png'
+import program_2 from '../../assets/program-2.png'
+import program_3 from "../../assets/program-3.png";
 
 const Program = () => {
   return (
-    <div>
-      
+    <div className="programs">
+      <div className="program">
+        <img src={program_1} alt="" />
+      </div>
+      <div className="program">
+        <img src={program_2} alt="" />
+      </div>
+      <div className="program">
+        <img src={program_3} alt="" />
+      </div>
     </div>
-  )
+  );
 }
 
 export default Program

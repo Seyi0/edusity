0e1a08090431b7b2aa33bef77e8f1111b69fcc2a
Seyi0 <makanjuolaseyi560@gmail.com>
2024-05-05 18:25:06 +0100

    getting started ::construction

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import Navbar from './components/navbar/Navbar'
+import Hero from './components/hero/Hero'
+import Program from './components/program/Program'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Hero/>
+      <Program/>
+    </div>
+  )
+}
+
+export default App

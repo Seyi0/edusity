cb6e1e15e470f103fa98a5a43822cc6137d762a5
Seyi0 <makanjuolaseyi560@gmail.com>
2024-05-07 19:01:57 +0100

    ongoing :construction:

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/navbar/Navbar'
 import Hero from './components/hero/Hero'
 import Program from './components/program/Program'
 import Title from './components/title/Title';
+import About from './components/about/About';
 
 const App = () => {
   return (
@@ -12,6 +13,8 @@ const App = () => {
       <div className="container">
         <Title subTitle="PROGRAMS AVAILABLE" title="Services been rendered" />
         <Program />
+        <About />
+        <Title subTitle="Gallery" title="Campus Photos" />
       </div>
     </div>
   );
--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,11 +1,38 @@
+/* eslint-disable react/no-unescaped-entities */
 import './About.css'
-
+import aboutImg from '../../assets/about.png'
+import playIcon from '../../assets/play-icon.png'
 const About = () => {
   return (
-    <div className='about'>
-      
+    <div className="about">
+      <div className="about-left">
+        <img src={aboutImg} className="about-img" />
+        <img src={playIcon} className="play-icon" />
+      </div>
+      <div className="about-right">
+        <h3>ABOUT UNIVERSITY</h3>
+        <h2>Nurturing Tomorrow's Leaders Today</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+        <p>
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+          dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          proident, sunt in culpa qui officia deserunt mollit anim id est
+          laborum.
+        </p>
+        <p>
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
+          ab illo inventore veritatis et quasi{" "}
+        </p>
+
+      </div>
     </div>
-  )
+  );
 }
 
 export default About
